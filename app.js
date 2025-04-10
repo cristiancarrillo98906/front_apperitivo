951f3bc682d40c36067c0c4fcffb92a9f84334be
FelipePC <cristiancarrillo98906@gmail.com>
2025-04-11 00:05:18 +0200

    Pendiente detallar pagina

--- app.js
+++ app.js
@@ -171,7 +171,7 @@ function cargarRestaurantes(){
 
             //EDIT BUTTON
             const editButton = document.createElement("button")
-            editButton.innerText = "Edit"
+            editButton.innerText = "Editar"
             editButton.id = "edit-button"
             editButton.onclick = function(){
                 window.location.href = "editRestaurante.html?id=" + restaurantes.id
@@ -180,7 +180,7 @@ function cargarRestaurantes(){
 
             //DELETE BUTTON
             const deleteButton = document.createElement("button")
-            deleteButton.innerText = "Delete"
+            deleteButton.innerText = "Eliminar"
             deleteButton.id = "delete-button"
             deleteButton.onclick = function(){
                 eliminarRestaurante(restaurantes.id)
@@ -190,7 +190,6 @@ function cargarRestaurantes(){
             row.appendChild(idCell)
             row.appendChild(nombreCell)
             row.appendChild(ubicacionCell)
-            
             row.appendChild(detallesCell)
             tbody.appendChild(row)
         }
@@ -252,6 +251,243 @@ function eliminarRestaurante(id){
         }
     })
 }
+
+function cargarRecetas(){
+    fetch("http://localhost:4050/recetas")
+    .then(function(respuestaTextoPlano){
+        return respuestaTextoPlano.json()
+    })
+    .then(function(data){
+        const tbody = document.getElementById("recetas-body")
+        for(let receta of data){
+            const row = document.createElement("tr")
+            const idCell = document.createElement("td")
+            idCell.innerText = receta.id
+
+            const tituloCell = document.createElement("td")
+            tituloCell.innerText = receta.titulo
+
+            const ingredientesCell = document.createElement("td")
+            ingredientesCell.innerText = receta.ingredientes
+            
+            //SHOW
+            const detallesCell = document.createElement("td")
+            const detallesButton = document.createElement("button")
+            detallesButton.innerText = "Ver"
+            detallesButton.classList.add("btn-detalles");
+            detallesButton.onclick = function(){
+                window.location.href = "showRecetas.html?id=" + receta.id
+            }
+            detallesCell.appendChild(detallesButton)
+
+            //EDIT BUTTON
+            const editButton = document.createElement("button")
+            editButton.innerText = "Editar"
+            editButton.id = "edit-button"
+            editButton.onclick = function(){
+                window.location.href = "editReceta.html?id=" + receta.id
+            }
+            detallesCell.appendChild(editButton)
+
+            //DELETE BUTTON
+            const deleteButton = document.createElement("button")
+            deleteButton.innerText = "Eliminar"
+            deleteButton.id = "delete-button"
+            deleteButton.onclick = function(){
+                eliminarReceta(receta.id)
+            }
+            detallesCell.appendChild(deleteButton)
+
+            row.appendChild(idCell)
+            row.appendChild(tituloCell)
+            row.appendChild(ingredientesCell)
+            row.appendChild(detallesCell)
+            tbody.appendChild(row)
+        }
+    })
+    .catch(function(error){
+        console.log("Error al obtener las recetas: ", error)
+    })
+}
+
+function eliminarReceta(id){
+    Swal.fire({
+        title:"¿Estás seguro?",
+        text:`Eliminar Receta '${id}'. Esta acción no se puede deshacer`,
+        icon:"warning",
+        showCancelButton:true,
+        confirmButtonText:"Sí, eliminar",
+        cancelButtonText:"Cancelar"
+    }).then(function(result){
+        if(result.isConfirmed){
+            //Si le hemos dado a "Sí, eliminar"
+            fetch("http://localhost:4050/recetas/" + id,{
+                method:"DELETE"
+            })
+            .then(function(respuestaTextoPlano){
+                return respuestaTextoPlano.json()
+            })
+            .then(function(data){
+                //OK
+                //data (String) y no JSON
+                console.log(data)
+                if(data.message.includes("Error")){
+                    Swal.fire({
+                        title: "Error!",
+                        text: data.message,
+                        icon: "error"
+                    })
+                } else {                
+                    Swal.fire({
+                        title: "Eliminado!",
+                        text: data.message,
+                        icon: "success"
+                    })
+                    .then(function(){
+                            //Refresco de página en JS desde el servidor y no desde caché
+                            window.location.reload(true)
+                            //cargarProductos()
+                        })
+                }                
+            })
+            .catch(function(error){
+                console.log(error)
+                Swal.fire({
+                    title: "¡Error!",
+                    text: error,
+                    icon: "error"
+                });
+            })
+        }
+    })
+}
+
+function cargarReseña(){
+    fetch("http://localhost:4050/review")
+    .then(function(respuetaTextoPlano){
+        //La respueta la transformo en JSON
+        return respuetaTextoPlano.json()
+        
+    })
+    .then(function(data){
+        console.log("Datos recibidos de la API:", data);
+        const tbody = document.getElementById("reseña-body")
+        for(let reseña of data){
+            const row = document.createElement("tr")
+            const idCell = document.createElement("td")
+            idCell.innerText = reseña.id
+
+            const usuarioCell = document.createElement("td")
+            usuarioCell.innerText = reseña.idUsuario
+
+            const restauranteCell = document.createElement("td")
+            restauranteCell.innerText = reseña.idRestaurante
+
+            const valoracionCell = document.createElement("td")
+            valoracionCell.innerText = reseña.valoracion
+
+            const fechaCell = document.createElement("td")
+            fechaCell.innerText = reseña.fecha
+
+            const detallesCel = document.createElement("td")
+            const detallesButton= document.createElement("button")
+            detallesButton.innerText="Ver"
+            detallesButton.id="ver"
+
+            detallesButton.classList.add("btn-detalles")
+            detallesButton.onclick= function(){
+                window.location.href="showResenya.html?id=" + reseña.id
+            }
+            const butonEditar= document.createElement("button")
+            butonEditar.innerText="Editar"
+            butonEditar.id="edit-button"
+            butonEditar.onclick= function(){
+                window.location.href="editResenya.html?id=" + reseña.id
+            }
+
+            
+            const deletebtn= document.createElement("button")
+            deletebtn.innerText="Eliminar"
+            deletebtn.id="delete-button"
+            deletebtn.onclick=function(){ eliminarReseña(reseña.id)}
+
+            
+            detallesCel.appendChild(detallesButton)
+            detallesCel.appendChild(butonEditar)
+            detallesCel.appendChild(deletebtn)
+
+            row.appendChild(idCell)
+            row.appendChild(usuarioCell)
+            row.appendChild(restauranteCell)
+
+            row.appendChild(valoracionCell)
+            row.appendChild(fechaCell)
+            row.appendChild(detallesCel)
+            tbody.appendChild(row)
+            console.log("Reseña procesada:", reseña);
+        }
+    })
+    .catch(function(error){
+        //Gestionamos el KO con "error" que tendrá formato JSON
+        console.log("Error al obtener las reseñas: ",error);
+    })
+}
+
+function eliminarReseña(id){
+    Swal.fire({
+        title:"¿Estás seguro?",
+        text:`Eliminar Reseña '${id}'. Esta acción no se puede deshacer`,
+        icon:"warning",
+        showCancelButton:true,
+        confirmButtonText:"Sí, eliminar",
+        cancelButtonText:"Cancelar"
+    }).then(function(result){
+        if(result.isConfirmed){
+            //Si le hemos dado a "Sí, eliminar"
+            fetch("http://localhost:4050/review/" + id,{
+                method:"DELETE"
+            })
+            .then(function(respuestaTextoPlano){
+                return respuestaTextoPlano.json()
+            })
+            .then(function(data){
+                //OK
+                //data (String) y no JSON
+                console.log(data)
+                if(data.message.includes("Error")){
+                    Swal.fire({
+                        title: "Error!",
+                        text: data.message,
+                        icon: "error"
+                    })
+                } else {                
+                    Swal.fire({
+                        title: "Eliminado!",
+                        text: data.message,
+                        icon: "success"
+                    })
+                    .then(function(){
+                            //Refresco de página en JS desde el servidor y no desde caché
+                            window.location.reload(true)
+                            //cargarProductos()
+                        })
+                }                
+            })
+            .catch(function(error){
+                console.log(error)
+                Swal.fire({
+                    title: "¡Error!",
+                    text: error,
+                    icon: "error"
+                });
+            })
+        }
+    })
+}
+
+
 // MAIN - Ejecución:
 cargarUsuarios()
 cargarRestaurantes()
+cargarRecetas()
+cargarReseña()
